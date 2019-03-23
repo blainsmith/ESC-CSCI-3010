90b9de2de33c4202dea604ec84ec2ed2690d8eb5
Blain Smith <rebelgeek@blainsmith.com>
2019-03-23 10:49:00 -0400

    final working program

--- program1/primes.asm
+++ program1/primes.asm
@@ -11,9 +11,6 @@ primesArray: times ARRAY_MAX_SIZE dd 1
     
     global primes
 
-; The arguments from primes are placed into registers edi, esi, edx.
-; 
-; The return value of the function is pulled from eax.
 primes:
     ; Read the argument from the C program and error if it is greater than 1000
     mov edi, [esp+4]
@@ -23,13 +20,60 @@ primes:
     ; Set the counter to zero
     mov ecx, 0
 fill:
-    mov [primesArray + edx * 4], edx
-    inc edx
-    cmp edx, ARRAY_SIZE + 1
+    ; Fill the array with their indicies
+    mov [primesArray + ecx * 4], ecx
+    inc ecx
+    cmp ecx, edi
     jne fill
 
-    mov eax, [primesArray + 1 * 4] ; move the result into the return register
-    ret                            ; retrun the sum
+    ; Rest the counter again, but start from 1 that increments to start from 2 at the next instruction
+    mov ecx, 1
+
+outerLoop:
+    ; Jump to the end after iterating the array
+    inc ecx
+    cmp ecx, edi
+    jge find
+
+    ; If the value is no zero,
+    ; copy the counter value to a new counter
+    mov esi, ecx
+
+innerLoop:
+    ; Copy the outer counter and
+    ; multiply it by the inner counter
+    mov eax, ecx
+    mul esi
+
+    ; If the product is larger than the range
+    ; return to the outer loop
+    cmp eax, edi
+    jge outerLoop
+
+    ; If the product is smaller than the range
+    ; set the multiple to zero, increment, and
+    ; loop again to the next multiple
+    mov edx, 0
+    mov [primesArray + eax * 4], edx
+    inc esi
+    cmp esi, edi
+    jl innerLoop
+
+find:
+    dec edi
+    mov ecx, edi
+
+findLoop:
+    ; Loop through the array in reverse finding the first non-zero number
+    mov eax, [primesArray + ecx * 4]
+    dec ecx
+    cmp eax, 0
+    je findLoop
+
+    ; The non-zero number is in eax which is the return register
+    ; so just return from the function
+    ret
+
 error:
     mov eax, -1
     ret
--- program1/primes.c
+++ program1/primes.c
@@ -15,9 +15,10 @@ int local_primes(){
         primes[i] = 1;
  
     for (i = 2;i < LIMIT; i++)
-        if (primes[i])
+        if (primes[i]) {
             for (j = i;i * j < LIMIT; j++)
                 primes[i * j] = 0;
+        }
  
     printf("\nPrime numbers in range 1 to %d are: \n", LIMIT);
     for (i = 2;i < LIMIT; i++)
@@ -28,13 +29,9 @@ int local_primes(){
 }
 
 int main() {
-    int a, rtn;
-	a = 10;
-	rtn = primes(a);
-    
-    printf("%d", rtn);
-
-    local_primes();
+    printf("largest prime under %d: %d\n", 1000, primes(1000));
+    printf("largest prime under %d: %d\n", 64, primes(64));
+    printf("largest prime under %d: %d\n", 10000, primes(10000));
 	
     return 0;
 }
